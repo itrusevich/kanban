05b3ab412b4f854bb6e80515db05e67146b29e99
Trusevich <ITrusevich@alluma.org>
2020-06-06 05:37:06 -0700

    Updates

---
+++ precache-manifest.5987615de7b2f978d10d8baff9334fda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b93af23f05a9924ec8c88ef000870911",
+    "url": "/kanban/index.html"
+  },
+  {
+    "revision": "6c665250818932bb4a7a",
+    "url": "/kanban/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "078794302c5aeb11dc23",
+    "url": "/kanban/static/css/main.c3510687.chunk.css"
+  },
+  {
+    "revision": "6c665250818932bb4a7a",
+    "url": "/kanban/static/js/2.b207f15f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kanban/static/js/2.b207f15f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "078794302c5aeb11dc23",
+    "url": "/kanban/static/js/main.9ebb18c0.chunk.js"
+  },
+  {
+    "revision": "976ef6cdb26dba17901e",
+    "url": "/kanban/static/js/runtime-main.a825c3b3.js"
+  }
+]);
